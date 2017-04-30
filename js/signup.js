7157a159d08668ea216b503c6f13810936cd9b2d
Anton Medinskiy <antonmedinskiy@gmail.com>
2017-04-30 18:30:09 +0300

    added validation for name, surname

--- js/signup.js
+++ js/signup.js
@@ -14,6 +14,7 @@
 			console.log("submit!!!!!!")
 			var form = $(this);
 			app.validateForm(form);
+			app.validateNames(form);
 		},
 
 		validateForm: function(form){
@@ -21,8 +22,8 @@
 			$.each(inputs, function(index,val) {
 				var input = $(val),
 					val = input.val(),
-					formGroup = input.parents('.form-group')
-					span = formGroup.find('span')
+					formGroup = input.parents('.form-group'),
+					span = formGroup.find('span');
 
 				if (val.length === 0) { 
 					formGroup.addClass('has-error')
@@ -31,6 +32,36 @@
 				else{ formGroup.removeClass('has-error')
 				span.text("")}
 			});
+		},
+		validateNames: function(form){
+			var validNames = true,
+				inputName = document.getElementById('inputName'),
+				inputSurname = document.getElementById('inputSurname'),
+				formGroupName = $(inputName).parents('.form-group')
+				formGroupSurname = $(inputSurname).parents('.form-group')
+				nameSpan = formGroupName.find('span')
+				surnameSpan = formGroupSurname.find('span')
+				re = new RegExp(/^[A-Za-zА-ЯЁа-яё\s]+$/);
+
+			if (re.test(inputName.value)) {
+				formGroupName.removeClass('has-error')
+				nameSpan.text("")
+				validNames == true
+			}
+			else if (inputName.value.length != 0) { formGroupName.addClass('has-error')
+				nameSpan.text("Имя должно содержать только буквы")
+				validNames == false
+			}
+			if (re.test(inputSurname.value)) {
+				formGroupSurname.removeClass('has-error')
+				surnameSpan.text("")
+				validNames == true
+			}
+			else if (inputSurname.value.length != 0) { formGroupSurname.addClass('has-error')
+				surnameSpan.text("Фамилия должно содержать только буквы")
+				validNames == false
+			}
+			return validNames
 		}	
 		
 	}
